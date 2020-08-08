42916c4898108b7496fd31eb9058c15ccff33f7e
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-08-08 10:09:39 -0700

    Updates

---
+++ precache-manifest.70761685fe2aec3e0ae33750cc01b4a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e4b3b2224b25475cae697bb5e6a23a",
+    "url": "/reactBlog/index.html"
+  },
+  {
+    "revision": "73793a0483574dc835ea",
+    "url": "/reactBlog/static/css/main.94a1af7e.chunk.css"
+  },
+  {
+    "revision": "eba90c1ee79701a54d81",
+    "url": "/reactBlog/static/js/2.ee7aa503.chunk.js"
+  },
+  {
+    "revision": "a6567c10c9a720d77d5051689542a5aa",
+    "url": "/reactBlog/static/js/2.ee7aa503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73793a0483574dc835ea",
+    "url": "/reactBlog/static/js/main.90955524.chunk.js"
+  },
+  {
+    "revision": "77afb4de3dc343413031",
+    "url": "/reactBlog/static/js/runtime-main.1b79a146.js"
+  }
+]);
